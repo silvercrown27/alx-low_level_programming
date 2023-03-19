e055857b4f76394ca47d9c71fc57948dcfbaf990
silvercrown27 <bradabwao14@gmail.com>
2023-03-19 15:54:46 -0700

    betty style coding number checker

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,32 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - this is the main function
+ *
+ * printf: dispay content
+ * Return: always 0 upon success
+ */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	/* YOUR CODE GOES HEAR */
 
-        if (n < 0)
-        {
-            printf("%d is negative", n);
-        }
-        else if (n == 0)
-        {
-            printf("%d is zero", n);
-        }
-        else
-        {
-            printf("%d is positive", n);
-        }
-        return (0);
+	if (n < 0)
+	{
+		printf("%d is negative\n");
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n");
+	}
+	else
+	{
+		printf("%d is positive\n");
+	return (0);
 }
