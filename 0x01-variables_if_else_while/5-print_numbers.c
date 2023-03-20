ce490a08d616dae21b47238c5c0f09da5b504579
silvercrown27 <bradabwao14@gmail.com>
2023-03-19 17:48:14 -0700

    iterate 1 through 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: alway 0 if run is success
+ */
+
+int main(void)
+{
+	for (int 1 = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
