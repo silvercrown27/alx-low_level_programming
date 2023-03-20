7db6e97c4772c1d263b5fd86faab617071f55810
silvercrown27 <bradabwao14@gmail.com>
2023-03-19 18:47:17 -0700

    print all occurrences of numbers btw 0-99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,12 +8,9 @@
 
 int main(void)
 {
-	int i = 0;
-	int j = 1;
-
-	for (; i < 99; i++)
+	for (int i = 0; i < 99; i++)
 	{
-		for (; j < 100; j++)
+		for (int j = 1; j < 100; j++)
 		{
 			printf("%02d", i);
 			printf(" ");
