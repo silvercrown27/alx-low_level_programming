a9e8207a51c5095310a4afb161d995119746790b
silvercrown27 <bradabwao14@gmail.com>
2023-03-20 08:45:54 -0700

    print all occurrences of numbers btw 0-99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,10 +20,6 @@ int main(void)
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
-			if (i == 98 && j == 99)
-			{
-				break;
-			}
 			putchar(',');
 			putchar(' ');
 			j++;
