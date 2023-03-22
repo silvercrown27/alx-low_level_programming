96f867d0ee310e74cde39fefb7410431366e3a24
silvercrown27 <bradabwao14@gmail.com>
2023-03-22 16:18:13 -0700

    print the last digit of a numner

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,11 @@
 
 int print_last_digit(int n)
 {
-	if( n >= 0)
-	{
-		n = n % 10;
-		_putchar(n + '0');
-	}
-	else
+	if (n < 0)
 	{
 		n = -(n);
-		n = n % 10;
-		_putchar(n + '0');
 	}
+	n = n % 10;
+	_putchar(n + '0');
 	return (n);
 }
