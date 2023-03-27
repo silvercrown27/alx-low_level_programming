27d5d4f130b836f4e955f1e57dba576eb98909e1
silvercrown27 <bradabwao14@gmail.com>
2023-03-27 16:29:43 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code and print string in reverse
+ * @s: parameter for the function
+ * Return: No return value
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[1] != 0)
+	{
+		i++;
+	}
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
