5e91a100143eec7049a1ba7f93775966e02b0ddc
silvercrown27 <bradabwao14@gmail.com>
2023-03-27 15:34:37 -0700

    swap function btn two pointera

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- *
+ * swap_int - Swap the values of two pinters with each other
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: no value
  */
 
 void swap_int(int *a, int *b)
