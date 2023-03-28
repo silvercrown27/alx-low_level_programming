0c7662d9785588578302925d37461857cfdf9e13
silvercrown27 <bradabwao14@gmail.com>
2023-03-27 17:26:17 -0700

    _reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - check the code and print string in reverse
+ * @s: parameter for the function
+ * Return: No return value
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	i--;
+	for (j = 0; j <= i ; i--, j++)
+	{
+		s[j] = s[i];
+	}
+}
