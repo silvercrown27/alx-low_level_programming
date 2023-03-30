200ffccb5de5e2e4d0ef3a43355bd8a6ae374634
silvercrown27 <bradabwao14@gmail.com>
2023-03-30 15:36:19 -0700

    prototypes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates strings
+ * @dest: string parameter 1
+ * @src: string parameter 2
+ * @n: parameter 3
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+}
