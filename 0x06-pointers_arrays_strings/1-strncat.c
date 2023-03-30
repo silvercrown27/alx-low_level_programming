ec61d7941492167bc028208a689e6fba750c14d6
silvercrown27 <bradabwao14@gmail.com>
2023-03-30 16:47:01 -0700

    string copy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,4 +10,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int i = 0;
+	int k = 0;
+	int j;
+
+	for (j = 0; dest[j] != '\0'; j++)
+	{
+		i++;
+	}
+	for (j = 0; src[j] != '\0'; j++)
+		k++;
+
+	for (j = 0; j < n && j < k; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -25,6 +25,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_buffer(char *b, int size);
 
-int strlength(char *s)
+int strlength(char *s);
 
 #endif
