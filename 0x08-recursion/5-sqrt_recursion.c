65306f0f8dd3c3c5cb69a63189695541d2a1d8ab
silvercrown27 <bradabwao14@gmail.com>
2023-04-05 16:48:48 -0700

    find aquaare

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int findsqrt(int n, int i);
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: integer input paramter
+ * Return: integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	i = 0;
+	return (findsqrt(n, i));
+}
+
+/**
+ *actual_sqrt_recursion - recurses to find the natural square root of a number
+ *@n: number to be square rooted
+ *@i: iterator
+ *Return: resulting square root
+ */
+
+int findsqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+
+	return (findsqrt(n, i + 1));
+}
