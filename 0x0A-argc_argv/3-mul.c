a260cfcc487cbbc6852e1239ed928be078400a0c
silvercrown27 <bradabwao14@gmail.com>
2023-04-10 17:09:55 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - function that print the product of two no
+ * @argc: parameter
+ * @argv: parameter arguments
+ * Return: 0 if success otherwise 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, k;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	j = atoi(argv[1]);
+	k = atoi(argv[2]);
+	
+	i = j * k;
+	printf("%d\n", i);
+	return (0);
+}
