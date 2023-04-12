6dad48238ce8c9acb12292272b3a0fb9652748f1
silvercrown27 <bradabwao14@gmail.com>
2023-04-11 19:00:49 -0700

    task 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_array - func that creates an array of chars
+ * @size: the size of the memory to assign
+ * @c: the address of the memory to assign
+ *
+ * Return: charcter or NULL if size = 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	s = malloc(sizeof(char) * size);
+	if (size == 0 || s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	
+	return (s);
+}
