8915b033e6574591082085a339cb6d1a786552b6
silvercrown27 <bradabwao14@gmail.com>
2023-04-13 15:34:19 -0700

    array memory alloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _calloc - function that allocates memory for an array
+ * @nmemb: input
+ * @size: size of the array
+ *
+ * Return: No value
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	int *arr;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		arr[0] = 0;
+	}
+	return (arr);
+}
