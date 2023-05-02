ee0cc3da2f16b5f15fd46f8c875c6ac0bb97e9c0
silvercrown27 <bradabwao14@gmail.com>
2023-05-02 16:10:02 -0700

    linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,10 @@
 size_t print_listint(const listint_t *h)
 {
 	int count = 0;
-	const listint_t *head = NULL;
+	const listint_t *head;
+
+	head = (listint_t*)malloc(sizeof(listint_t));
+	head = NULL;
 
 	if (h == NULL)
 		return (count);
