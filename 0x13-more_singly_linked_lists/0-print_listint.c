085590d09b83e6ef0879afb031ec83624fa89274
silvercrown27 <bradabwao14@gmail.com>
2023-05-02 08:47:15 -0700

    count nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that counts the number of nodes in the list
+ * @h: input
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+	const listint_t *head = NULL;
+
+	if (h == NULL)
+		return (count);
+
+	head = h;
+
+	while (head != NULL)
+	{
+		count++;
+		printf("%d\n", head->n);
+		head = head->next;
+	}
+	return (count);
+}
