63249a425162ddce89c7ffa89b4baa08f8cad69e
silvercrown27 <bradabwao14@gmail.com>
2023-05-04 16:13:51 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,4 @@
-#include "Main.h"
+#include "main.h"
 
 /**
  * print_binary - function that converts a number to its binary equivilant
@@ -9,10 +9,17 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n > 0)
 	{
-		print_binary(n >> 1);
+		if (n == 1)
+		{
+			putchar(n + '0');
+			return;
+		}
 
+		print_binary(n >> 1);
 		putchar((n & 1) ? '1' : '0');
 	}
+	if (n == 0)
+		putchar(n + '0');
 }
